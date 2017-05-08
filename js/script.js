3554354475504f56e8ee7f3924f4fde11eae1169
jamescarbone24 <jamescarbone@Jamess-MacBook-Air.local>
2017-05-08 18:34:41 -0400

    added scripts and pseudoCode

---
+++ js/script.js
@@ -0,0 +1,26 @@
+// PeusdoCode
+
+// index page
+// - nav bar hover
+
+// brother of the month page
+// - nav bar hover
+// - click on picture, come up with discription
+
+// event page
+// - nav bar hover
+// - click on picture, come up with discription
+
+// causes page
+// - nav bar hover
+// - click on picture, come up with discription
+
+// FAQS page
+// - nav bar hover
+// - click on each question, get the answer and closed all other answers
+// - form fill for other questions, get value
+
+$(document).ready(function() {
+	
+
+});
